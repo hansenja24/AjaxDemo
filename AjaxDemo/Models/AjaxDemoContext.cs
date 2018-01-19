5c6d88500ca5fe4e0c55bee314d7778ce6a05a22
Hansen <hansenja24@gmail.com>
2018-01-19 13:39:21 -0800

    create ajax demo

---
+++ AjaxDemo/Models/AjaxDemoContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace AjaxDemo.Models
+{
+    public class AjaxDemoContext : DbContext
+    {
+        public AjaxDemoContext()
+        {
+        }
+
+        public DbSet<Destination> Destination { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySql(@"Server=localhost;Port=8889;database=AjaxDemo;uid=root;pwd=root;");
+        }
+
+        public AjaxDemoContext(DbContextOptions<AjaxDemoContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
